dc20738d99c73ed009b372305998c9e0bc62a3de
casanovajose <jose.casanova@nayra.coop>
2020-09-04 15:31:54 -0300

    epochs loop

--- lib/linreg/model.ex
+++ lib/linreg/model.ex
@@ -11,23 +11,27 @@ defmodule Linreg.Model do
     b + m * x
   end
 
-  def train(%Model{m: m, b: b} = model, %Linreg.Data{points: points}, opts \\ []) do
+  def train(%Model{} = model, %Linreg.Data{points: points}, opts \\ []) do
     learning_rate = Keyword.get(opts, :learning_rate, 0.01)
+    epochs = Keyword.get(opts, :epochs, 100)
 
-    # mean error of m
-    m_error =
-      points
-      |> Enum.map(fn {x, y} -> x* (predict(model, x) - y) end)
-      |> Enum.sum()
-      |> Kernel./(length(points))
+    for _epoch <- 1..epochs, reduce: model do
+      %Model{m: m, b: b} = model ->
+         # mean error of m
+        m_error =
+          points
+          |> Enum.map(fn {x, y} -> x* (predict(model, x) - y) end)
+          |> Enum.sum()
+          |> Kernel./(length(points))
 
-    # mean error of b
-    b_error =
-      points
-      |> Enum.map(fn {x, y} -> predict(model, x) - y end)
-      |> Enum.sum()
-      |> Kernel./(length(points))
+        # mean error of b
+        b_error =
+          points
+          |> Enum.map(fn {x, y} -> predict(model, x) - y end)
+          |> Enum.sum()
+          |> Kernel./(length(points))
 
-      %Model{model | m: m - m_error * learning_rate, b: b - b_error * learning_rate}
+          %Model{model | m: m - m_error * learning_rate, b: b - b_error * learning_rate}
+    end
   end
 end

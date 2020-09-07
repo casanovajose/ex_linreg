fbc9b4289a38710e069a1623efe988ade13aa8b7
casanovajose <jose.casanova@nayra.coop>
2020-09-07 20:44:12 -0300

    adding prediction line

--- lib/linreg_web/live/regression_live.ex
+++ lib/linreg_web/live/regression_live.ex
@@ -8,6 +8,9 @@ defmodule LinregWeb.RegressionLive do
       socket
       |> assign(model: Model.new())
       |> assign(data: Data.new())
+      |> assign(start_y: 0)
+      |> assign(end_y: 0)
+
     IO.inspect(params, label: "params mount")
     {:ok, socket}
   end
@@ -24,7 +27,27 @@ defmodule LinregWeb.RegressionLive do
       |> Data.add_point(map(x, 0, 800, 0, 1), map(y, 0, 800, 0, 1))
 
       IO.inspect(data, label: "mapped points")
-      assign(socket, data: data)
+      #assign(socket, data: data)
+
+      socket
+      |> assign(data: data)
+      |> learn()
+      |> update_prediction()
+  end
+
+  defp learn(%{assigns: %{data: data, model: model}} = socket) do
+    if length(data.points) >= 2 do
+      model = Model.train(model, data, learning_rate: 0.1, epochs: 500)
+      assign(socket, model: model)
+    else
+      socket
     end
+  end
+
+  defp update_prediction(%{assigns: %{model: model}} = socket) do
+    socket
+    |> assign(start_y: Model.predict(model, 0))
+    |> assign(end_y: Model.predict(model, 1))
+  end
 
   end
